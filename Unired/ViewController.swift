ea812ffbe23370365cb6e9c45839b58617e1ff6a
Unired203 <66329624+Unired203@users.noreply.github.com>
2020-06-03 23:57:42 +0500

    Initial commit

---
+++ Unired/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by ~Akhtamov on 5/18/20.
+//  Copyright © 2020 ~Akhtamov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
